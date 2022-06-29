9770b1044bed92057b809ebaafcf75726638ebf9
Benjamin-Re <renker.benjamin@gmail.com>
2022-06-29 21:56:14 +0200

    Frontend: Add fetch to update product quantity in stock

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,18 +3,18 @@ import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
 export function Cart() {
-  const { cart, logged, token, userId, getQuantity, increase, decrease } = useMyContext();
+  const { cart, logged, token, userId, getQuantity, increase, decrease } =
+    useMyContext();
   const [getCart, setCart] = cart;
   const [getLoggedIn, setLoggedIn] = logged;
   const [getUserId, setUserId] = userId;
   const [getToken, setToken] = token;
-  
 
   const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   console.log("render");
-  // }, [getCart]);
+  useEffect(() => {
+    console.log(getCart);
+  }, [getCart]);
 
   return (
     <>
@@ -24,17 +24,30 @@ export function Cart() {
           return (
             <li>
               {product.name}, {product.price}€, {product.quantity} units,
-              <button onClick={()=>{increase(product.id)}}>+</button> 
-              <button onClick={()=>{decrease(product.id)}}>-</button> 
+              <button
+                onClick={() => {
+                  increase(product.id);
+                }}
+              >
+                +
+              </button>
+              <button
+                onClick={() => {
+                  decrease(product.id);
+                }}
+              >
+                -
+              </button>
             </li>
           );
         })}
       </ul>
+      <div className="total">Total: {getTotal()}</div>
       <button onClick={handleCheckout}>Checkout</button>
     </>
   );
-      
-  // function removePost(product) {   
+
+  // function removePost(product) {
   //   let itemQuant = getQuant(1);
   //   console.log(itemQuant);
   //   // setCart(prevState=>{
@@ -49,6 +62,11 @@ export function Cart() {
   //   // });
   // }
 
+  function getTotal() {
+    return getCart.reduce((total, current) => {
+      return (total += current.price * current.quantity);
+    }, 0);
+  }
 
   function handleCheckout() {
     console.log(getToken);
@@ -68,9 +86,7 @@ export function Cart() {
     let order = {
       date: new Date(),
       products: getCart,
-      total: getCart.reduce((total, current) => {
-        return (total += (current.price * current.quantity));
-      }, 0),
+      total: getTotal(),
     };
     // Current user's id
     let userId = getUserId;
@@ -80,21 +96,45 @@ export function Cart() {
     // https://enigmatic-temple-40493.herokuapp.com
     fetch("http://localhost:8000/users/" + userId, {
       method: "PATCH",
-      headers: { 
+      headers: {
         "Content-Type": "application/json",
-        "Authorization": `bearer ${getToken}` 
+        Authorization: `bearer ${getToken}`,
       },
       body: JSON.stringify(order),
     })
       .then((res) => {
-        if(!res.ok) {throw new Error("errpr")}
+        if (!res.ok) {
+          throw new Error("errpr");
+        }
         return res.json();
       })
-      .then((data) => {
-        // Clear the cart after successful checkout
-        setCart([]);
-        console.log(data);
-      })
+      .then((data) => {})
       .catch((error) => console.log(error));
+
+    // Decrease products quantity in stock
+    getCart.map((product) => {
+      const id = product.id;
+      const quantity = product.quantity * -1;
+      fetch("http://localhost:8000/products/" + id, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `bearer ${getToken}`,
+        },
+        body: JSON.stringify({ quantity: quantity }),
+      })
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error("errpr");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          // Clear the cart after successful checkout
+          setCart([]);
+          console.log(data);
+        });
+    });
+    // navigate("/my-orders");
   }
 }
